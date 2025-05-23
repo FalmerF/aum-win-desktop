e112b4786670515c73eb520f44cf32a517763e00
Falmer <foulorown@gmail.com>
2025-05-23 15:06:36 +0300

    Add JavaFX

--- src/main/java/ru/ilug/aumwindesktop/AumWinDesktopApplication.java
+++ src/main/java/ru/ilug/aumwindesktop/AumWinDesktopApplication.java
@@ -1,5 +1,12 @@
 package ru.ilug.aumwindesktop;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -7,10 +14,27 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class AumWinDesktopApplication {
+public class AumWinDesktopApplication extends Application {
+
+    private static String[] args;
 
     public static void main(String[] args) {
-        SpringApplication.run(AumWinDesktopApplication.class, args);
+        AumWinDesktopApplication.args = args;
+        Application.launch(AumWinDesktopApplication.class, args);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+
+        SpringApplication.run(AumWinDesktopApplication.class, args);
+    }
 }
