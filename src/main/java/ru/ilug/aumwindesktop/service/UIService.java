1b4dfcd520756cd6da412a7992d2186404b2c02a
Falmer <foulorown@gmail.com>
2025-05-29 14:24:40 +0300

    - Save/load access token - User avatar

--- src/main/java/ru/ilug/aumwindesktop/service/UIService.java
+++ src/main/java/ru/ilug/aumwindesktop/service/UIService.java
@@ -11,18 +11,25 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.ilug.aumwindesktop.data.model.ApplicationStatistic;
+import ru.ilug.aumwindesktop.data.model.User;
 import ru.ilug.aumwindesktop.web.ServiceWebApi;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UIService {
@@ -36,39 +43,87 @@ public class UIService {
 
     @PostConstruct
     public void init() {
-        FlowPane root = new FlowPane();
+        VBox root = new VBox();
 
-        Button loginButton = new Button("Login via GitHub");
-        loginButton.setDefaultButton(true);
-        loginButton.setAlignment(Pos.CENTER);
-        loginButton.setOnMouseClicked(event -> {
-            AuthorizationFlowService authFlow = context.getBean(AuthorizationFlowService.class);
-            authFlow.start();
-        });
-
-        VBox vbox = new VBox();
-        vbox.setPadding(new Insets(10));
-        vbox.prefWidthProperty().bind(root.widthProperty());
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setStyle("-fx-background-color: -color-base-1;");
-        vbox.getChildren().add(loginButton);
-
-        tableView = new TableView<>();
-        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        tableView.prefHeightProperty().bind(root.heightProperty());
-        tableView.prefWidthProperty().bind(root.widthProperty());
-
-        TableColumn<ApplicationStatistic, String> exePathColumn = new TableColumn<>("Exe Path");
-        exePathColumn.setCellValueFactory(new PropertyValueFactory<>("exePath"));
-        tableView.getColumns().add(exePathColumn);
-
-        TableColumn<ApplicationStatistic, String> timeColumn = new TableColumn<>("Time");
-        timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
-        tableView.getColumns().add(timeColumn);
+        VBox vbox = createHeader(root);
+        tableView = createStatisticTable(root);
 
         root.getChildren().addAll(vbox, tableView);
 
         Platform.runLater(() -> scene.setRoot(root));
+    }
+
+    private VBox createHeader(VBox root) {
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(10));
+        vbox.prefWidthProperty().bind(root.widthProperty());
+        vbox.setAlignment(Pos.CENTER_RIGHT);
+        vbox.setStyle("-fx-background-color: -color-base-1;");
+
+        UserService userService = context.getBean(UserService.class);
+        if(userService.isAuthorized()) {
+            vbox.getChildren().add(createAvatar(userService.getUser()));
+        } else {
+            vbox.getChildren().add(createLoginButton());
+        }
+
+        return vbox;
+    }
+
+    private Button createLoginButton() {
+        Button button = new Button("Login via GitHub");
+        button.setDefaultButton(true);
+        button.setAlignment(Pos.CENTER_RIGHT);
+        button.setOnMouseClicked(event -> {
+            AuthorizationFlowService authFlow = context.getBean(AuthorizationFlowService.class);
+            authFlow.start();
+        });
+
+        return button;
+    }
+
+    private HBox createAvatar(User user) {
+        Text name = new Text(user.getName());
+
+        ImageView imageView = new ImageView(user.getAvatarUrl());
+        imageView.setFitWidth(30);
+        imageView.setFitHeight(30);
+
+        Circle clip = new Circle();
+        clip.setRadius(Math.min(imageView.getFitWidth(), imageView.getFitHeight()) / 2);
+        clip.setCenterX(imageView.getFitWidth() / 2);
+        clip.setCenterY(imageView.getFitHeight() / 2);
+
+        imageView.setClip(clip);
+
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER_RIGHT);
+        hbox.setSpacing(8);
+        hbox.getChildren().addAll(name, imageView);
+
+        return hbox;
+    }
+
+    @SuppressWarnings("unchecked")
+    private TableView<ApplicationStatistic> createStatisticTable(VBox root) {
+        TableView<ApplicationStatistic> tableView = new TableView<>();
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tableView.prefWidthProperty().bind(root.widthProperty());
+
+        tableView.getColumns().addAll(
+                createStatisticTableColumn("exePath", "Exe Path"),
+                createStatisticTableColumn("time", "Time")
+        );
+
+        VBox.setVgrow(tableView, Priority.ALWAYS);
+
+        return tableView;
+    }
+
+    private TableColumn<ApplicationStatistic, String> createStatisticTableColumn(String name, String displayName) {
+        TableColumn<ApplicationStatistic, String> column = new TableColumn<>(displayName);
+        column.setCellValueFactory(new PropertyValueFactory<>(name));
+        return column;
     }
 
     @Scheduled(fixedRate = 15, timeUnit = TimeUnit.SECONDS)
--- src/main/java/ru/ilug/aumwindesktop/service/UserService.java
+++ src/main/java/ru/ilug/aumwindesktop/service/UserService.java
@@ -1,40 +1,96 @@
 package ru.ilug.aumwindesktop.service;
 
+import com.sun.jna.platform.win32.Crypt32Util;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import ru.ilug.aumwindesktop.data.model.User;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Slf4j
 @Service
 public class UserService {
 
+    private static final Path TOKEN_PATH = Path.of("./token");
+
     private final WebClient githubClient;
 
     private String token;
+    @Getter
+    private User user;
 
     public UserService() {
         this.githubClient = WebClient.builder()
                 .baseUrl("https://api.github.com")
                 .build();
+
+        token = loadTokenFromSecureFile();
+
+        if (token != null) {
+            updateUserInformation();
+        }
     }
 
     public void setToken(String token) {
         this.token = token;
+
+        try {
+            saveTokenToSecureFile();
+        } catch (Exception e) {
+            log.error("Error on save access token");
+        }
+
         updateUserInformation();
     }
 
-    private void updateUserInformation() {
-        User user = githubClient.get()
-                .uri("/user")
-                .headers(headers -> headers.setBearerAuth(token))
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(User.class)
-                .block();
+    public boolean isAuthorized() {
+        return token != null && user != null;
+    }
 
-        log.info("User info: {}", user);
+    private void saveTokenToSecureFile() throws IOException {
+        byte[] encryptedData = Crypt32Util.cryptProtectData(token.getBytes());
+
+        if (Files.exists(TOKEN_PATH)) {
+            Files.delete(TOKEN_PATH);
+        }
+
+        Files.write(TOKEN_PATH, encryptedData);
+    }
+
+    private String loadTokenFromSecureFile() {
+        if (!Files.exists(TOKEN_PATH)) {
+            return null;
+        }
+
+        try {
+            byte[] encryptedData = Files.readAllBytes(TOKEN_PATH);
+            byte[] data = Crypt32Util.cryptUnprotectData(encryptedData);
+            return new String(data, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private void updateUserInformation() {
+        try {
+            user = githubClient.get()
+                    .uri("/user")
+                    .headers(headers -> headers.setBearerAuth(token))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(User.class)
+                    .block();
+        } catch (Exception e) {
+            log.error("Error on get user information", e);
+            user = null;
+        }
     }
 
 }
